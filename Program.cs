af2e1df30701da9acaff68c43e1308449775e7a0
Kermid <jakub.gibowski312@gmail.com>
2024-08-08 15:40:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace papier_kamien
+{
+    internal class Program
+    {
+        /*const int papier = 1;
+        const int kamien = 2;
+        const int nozyce = 3;*/
+
+        public static Random rnd = new Random();
+        
+        static void Main(string[] args)
+        {
+           bool kontynuuj = true;
+            while (kontynuuj)
+            {
+                Console.WriteLine("PAPIER/KAMIEN/NOŻYCE!");
+                Console.WriteLine("Wybierz jedną z trzech opcji");
+                Console.WriteLine("Papier = 1");
+                Console.WriteLine("Kamień = 2");
+                Console.WriteLine("Nożyce = 3");
+                try
+                {
+                    int wyborGracza = int.Parse(Console.ReadLine());
+                    int wylosowanaWartosc = rnd.Next(1, 4);
+
+                    logikaGry.kamienPapier(wylosowanaWartosc, wyborGracza);
+
+                }
+                catch (Exception)
+                {
+                    
+                    Console.WriteLine("Nieprawidłowy znak.Wybór jest z zakresu liczb 1-3");
+                }
+                
+            }
+        }
+    }
+}
---
+++ logikaGry.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace papier_kamien
+{
+    internal class logikaGry
+    {
+       public static void kamienPapier(int wylosowanaWartosc, int wyborGracza)
+        {
+            Console.Write($"{wylosowanaWartosc} ");
+           switch(wyborGracza)
+            {
+                case 1://papier
+                    if(wylosowanaWartosc == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("Papier na papier.Remis!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (wylosowanaWartosc == 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Papier na kamień.Wygrywasz!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (wylosowanaWartosc == 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Papier na nożyce.Wygrywa przeciwnik!");
+                        Console.ForegroundColor= ConsoleColor.White;
+                    }
+                        break;
+                case 2://kamien
+                    if (wylosowanaWartosc == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Kamień na papier.Wygrywa przeciwnik!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (wylosowanaWartosc == 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("Kamień na kamień.Remis!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (wylosowanaWartosc == 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Kamień na nożyce.Wygrywasz!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    break;
+                case 3://nozyce
+                    if (wylosowanaWartosc == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Nożyce na papier.Wygrywasz!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (wylosowanaWartosc == 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Nożyce na kamień.Przegrywasz!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (wylosowanaWartosc == 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("Nożyce na nożyce.Remis!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    break;
+            }
+
+        }
+    }
+}
